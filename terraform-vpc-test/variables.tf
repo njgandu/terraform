fe1ddd8c2519b8e9dee1846ad9e5bb69ff86f9af
jyothi <prakashjyothi1416@gmail.com>
2024-11-19 22:15:24 +0530

    adding vpc module

---
+++ terraform-vpc-test/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name"{
+    default = "expense"
+}
+
+variable "common_tags"{
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+
+    }
+}
+
+variable "public_subnet_cidrs"{
+    default = ["10.110.10.0/24","10.110.11.0/24"]
+}
+
+variable "private_subnet_cidrs"{
+    default = ["10.110.20.0/24","10.110.21.0/24"]
+}
+
+variable "database_subnet_cidrs"{
+    default = ["10.110.30.0/24","10.110.31.0/24"]
+}
