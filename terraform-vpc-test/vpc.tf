7a6a620ff02f31f9974ab7dd7ca17fc1d082af5e
jyothi <prakashjyothi1416@gmail.com>
2024-11-19 22:41:44 +0530

    aws vpc modile

--- terraform-vpc-test/vpc.tf
+++ terraform-vpc-test/vpc.tf
@@ -1,9 +1,11 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "https://github.com/njgandu/terraform-aws-vpc.git"
+    source = "git::https://github.com/njgandu/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
 }
+
+##https://github.com/njgandu/terraform-aws-vpc.git
